c7e75c16b85ea2c93d407d7a6957fe5760ce5b2c
scupak <anto1095@easv365.dk>
2021-04-26 00:41:01 +0200

    add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) }, { path: 'collections', loadChildren: () => import('./collections/collections.module').then(m => m.CollectionsModule) }, { path: 'items', loadChildren: () => import('./items/items.module').then(m => m.ItemsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
