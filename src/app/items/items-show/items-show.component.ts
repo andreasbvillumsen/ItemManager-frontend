0d2f78f55caa1a07f7bc4ca9a7b09eb7506252a1
Andreas <55873124+CloseToDie@users.noreply.github.com>
2021-05-06 06:16:40 +0200

    Added tailwind and a bit of design

---
+++ src/app/items/items-show/items-show.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-items-show',
+  templateUrl: './items-show.component.html',
+  styleUrls: ['./items-show.component.scss']
+})
+export class ItemsShowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
