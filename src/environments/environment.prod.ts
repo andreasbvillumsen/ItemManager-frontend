d8ed04e39e3a8b1b0cb65238474f002afb7f37fd
kasp608b <54952896+kasp608b@users.noreply.github.com>
2021-05-12 10:35:52 +0200

    nani

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:2400',
+  apiUrl: 'http://localhost:3400',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:2400',
+  apiUrl: 'http://localhost:3400',
 };
 
 /*
