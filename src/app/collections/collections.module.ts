b8da121878546c201c36f7e1283d12f74cee6cb0
Andreas <55873124+CloseToDie@users.noreply.github.com>
2021-05-20 13:56:52 +0200

    Removed some old code

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -8,16 +8,10 @@ import {NgxsModule} from '@ngxs/store';
 import {ItemsModule} from '../items/items.module';
 import {ItemState} from '../items/state/items.state';
 import {ReactiveFormsModule} from '@angular/forms';
-import {UploadFormComponent} from './shared/components/upload-form/upload-form.component';
-import {UploadDetailsComponent} from './shared/components/upload-details/upload-details.component';
-import {UploadListComponent} from './shared/components/upload-list/upload-list.component';
 
 @NgModule({
   declarations: [
     CollectionsComponent,
-    UploadFormComponent,
-    UploadDetailsComponent,
-    UploadListComponent
   ],
   imports: [
     CommonModule,
